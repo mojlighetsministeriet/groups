7405227f399dca9652ab4e57f95a6b15d9630180
Fredrik Söderström <fredrik.soderstrom@mojlighetsministeriet.se>
2017-10-01 21:08:48 +0200

    Fix group invitation foreign keys

--- entity/groupinvitation.go
+++ entity/groupinvitation.go
@@ -2,10 +2,12 @@ package entity
 
 import uuid "github.com/satori/go.uuid"
 
+// GroupInvitation is used to keep track of one account that invited another to a group
 type GroupInvitation struct {
-	ID        string `json:"id" gorm:"not null;unique;size:36" validate:"uuid4,required"`
-	Group     Group  `gorm:"not null;size:36"`
-	AccountID string `json:"accountId" gorm:"not null;size:36" validate:"uuid4,required"`
+	ID            string `json:"id" gorm:"not null;unique;size:36" validate:"uuid4,required"`
+	GroupID       string `json:"groupId" gorm:"not null;size:36" validate:"uuid4,required"`
+	ToAccountID   string `json:"toAccountId" gorm:"not null;size:36" validate:"uuid4,required"`
+	FromAccountID string `json:"fromAccountId" gorm:"not null;size:36" validate:"uuid4,required"`
 }
 
 // BeforeSave will run before the struct is persisted with gorm
